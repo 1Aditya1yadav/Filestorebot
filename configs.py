bcf53e0d7ff1d1ecf2e2e592b9bc2f0efff5a6b7
Aditya yadav <92971784+1Aditya1yadav@users.noreply.github.com>
2022-11-12 19:59:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,18 +1,16 @@
-# (c) @PredatorHackerzZ || @TeleRoidGroup
-
 import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	API_ID = int(os.environ.get("19207498", "0"))
+	API_HASH = os.environ.get("174c00bab9c841383cee419c3da13651")
+	BOT_TOKEN = os.environ.get("5661182742:AAGQEawDxbVJ--nixJtUHIprh_bpOAwo2Bs")
+	BOT_USERNAME = os.environ.get("@Savefileherebot ")
+	DB_CHANNEL = int(os.environ.get("-1001834516148 ", "-100"))
+	BOT_OWNER = int(os.environ.get("1444300019", "1445283714"))
+	DATABASE_URL = os.environ.get("mongodb+srv://Aditya65:<Ab 9596 ek>@cluster0.37y2cxz.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get(" -1001806508673", "")
+	LOG_CHANNEL = os.environ.get(" -1001705724548 ", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
